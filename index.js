1b940ebc574c03ba68c50b8708ca217fc0f4c8de
tristonpalacios <triston.j.palacios@gmail.com>
2022-02-25 10:33:36 -0700

    Added homepage and views file for ejs

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-const express = require('express')
-const ejsLayouts = require('express-ejs-layouts')
+const express = require('express')//import express
+const ejsLayouts = require('express-ejs-layouts')//import ejs layous
+require('dotenv').config()//allow access to env vars
 
-
-const app = express()
+const app = express()//create instance of express
+//check for env port if not, use 3000
 const port = process.env.PORT || 3000
 
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs')//set the view view engine to ejs
 
 app.use(express.urlencoded({ extended: false }))
-app.use(ejsLayouts)
+app.use(ejsLayouts)//tell express to use ejs layouts
 
 // middleware
 
 
 // GET / - display all articles and their authors
 app.get('/', (req, res) => {
-  res.send(`Hello Chads`)
+  res.render(`home.ejs`)
   
 })
 
-
+//
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
 })
